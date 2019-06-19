ce4f3a8ad96e64c032d1049e828a7763e4363eb0
Душица Јанковиќ <dusica.jankovik@gmail.com>
2019-06-19 02:42:12 +0200

    icon

--- FrozenWorld/Properties/Resources.Designer.cs
+++ FrozenWorld/Properties/Resources.Designer.cs
@@ -421,6 +421,16 @@ namespace FrozenWorld.Properties {
         }
         
         /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap Snowflake1 {
+            get {
+                object obj = ResourceManager.GetObject("Snowflake1", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Icon similar to (Icon).
         /// </summary>
         internal static System.Drawing.Icon snowflake1_d2ebjF {
--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# <img src="https://github.com/djankovik/FrozenWorld/blob/master/FrozenWorld/Resources/snowflake1_d2ebjF.ico" alt="icon" width="25px"/> FrozenWorld 
+# <img src="https://github.com/djankovik/FrozenWorld/blob/master/FrozenWorld/Resources/Snowflake1.jpg" alt="icon" width="25px"/> FrozenWorld 
 ## by Dushica Jankovikj 161509
